d3f7680f276291c6081fc2ddf3653db7b7b8c3a1
Stéphane Potti <stephane.potti@ul.com>
2022-03-03 14:28:26 +0100

    Update HexData 	- correct bug on big integer 	- add override methods (add, eq, setitem, ...)

--- pyhexdata/HexData.py
+++ pyhexdata/HexData.py
@@ -8,7 +8,7 @@ class HexData:
     HexData class
     """
 
-    version = "0.2.0"
+    version = "0.1.0"
     __version__ = version
 
     def __init__(self, value=None, padding=None):
@@ -27,8 +27,12 @@ class HexData:
         if not(value is None):
             if isinstance(value, np.ndarray):
                 self._data = value.astype(np.uint8)
+            elif isinstance(value, list):
+                self._data = np.array(value, dtype=np.uint8)
             elif isinstance(value, bytes):
                 self._data = np.frombuffer(value, dtype=np.uint8)
+            elif isinstance(value, np.uint8):
+                self._data = np.array([value], dtype=np.uint8)
             elif isinstance(value, str):
                 value = value.replace(" ", "")
 
@@ -38,8 +42,8 @@ class HexData:
                 # String read
                 byte_value = bytes.fromhex(value)
                 self._data = np.frombuffer(byte_value, dtype=np.uint8)
-            elif isinstance(value, int):
-                nr_bytes = math.ceil(value.bit_length() / 8.)
+            elif isinstance(value, int) or isinstance(value, int):
+                nr_bytes = max(1, math.ceil(value.bit_length() / 8.))
 
                 # Integer read
                 byte_value = value.to_bytes(nr_bytes, "big", signed=False)
@@ -75,6 +79,91 @@ class HexData:
                 return " ".join(["{:02x}".format(x) for x in self._data])
             else:
                 return "".join(["{:02x}".format(x) for x in self._data])
+        return None
+
+    def to_send_cmd_string(self, extended_LC=False):
+        """
+        This function converts the numpy array to string with spaces
+
+        Input:
+           - spaces (bool; optional): if True, spaces between each byte, without spaces either
+        Output:
+           - None
+        """
+        apdu_format = ""
+        cmd = ""
+        index = 0
+        if not(self._data is None):
+            ln = self._data.shape[0]
+
+            # CLA
+            apdu_format += "CLA"
+            cmd +=         " {:02x}".format(self._data[index])
+            index += 1
+
+            # INS
+            apdu_format += " | INS"
+            cmd +=         " |  {:02x}".format(self._data[index])
+            index += 1
+
+            # P1 P2
+            apdu_format += " | P1 P2"
+            cmd +=         " | {0:02x} {1:02x}".format(self._data[index], self._data[index+1])
+            index += 2
+
+            # DATA
+            if ln > index:
+                if extended_LC:
+                    # extended_LC
+                    apdu_format += " |    LC"
+                    cmd +=         " | {0:02x} {1:02x}".format(self._data[index], self._data[index+1])
+                    index += 2
+                else:
+                    # LC
+                    apdu_format += " | LC"
+                    cmd += " | {:02x}".format(self._data[index])
+                    index += 1
+
+                # DATA
+                apdu_format += " | " + " "*(3*(ln-index) - 3) + "DATA"
+                cmd += " |   "
+                cmd +=         " ".join(["{:02x}".format(self._data[x]) for x in range(index, ln)])
+
+            apdu_format += " | "
+            cmd += " | "
+
+            return apdu_format, cmd
+
+        return None
+
+    def to_cmd_resp_string(self):
+        """
+        This function converts the numpy array to string with spaces
+
+        Input:
+           - spaces (bool; optional): if True, spaces between each byte, without spaces either
+        Output:
+           - None
+        """
+        apdu_format = ""
+        cmd = ""
+        if not(self._data is None):
+            ln = self._data.shape[0]
+
+            if ln > 2:
+                # DATA
+                apdu_format += " "*(3*(ln-2) - 3) + "DATA | "
+                cmd += "  " + " ".join(["{:02x}".format(self._data[x]) for x in range(0, ln-2)]) + " | "
+            elif ln == 2:
+                pass
+            else:
+                return apdu_format, cmd
+
+            apdu_format += "SW1 | SW2"
+            cmd += " {0:02x} |  {1:02x}".format(self._data[-2], self._data[-1])
+
+            return apdu_format, cmd
+
         return None
 
     def to_bytes(self):
@@ -120,6 +209,23 @@ class HexData:
         else:
             pass
 
+    def right_padding(self, nr_bytes):
+        """
+        This function forces the padding of the _data attribute internally
+
+        Input:
+           - nr_bytes (int): the number of bytes of the _data attribute
+        Output:
+           - None
+        """
+        diff = nr_bytes - self._data.shape[0]
+        if diff > 0:
+            self._data = np.concatenate((self._data, np.zeros(diff, dtype=np.uint8)))
+        elif diff < 0:
+            self._data = self._data[:-diff]
+        else:
+            pass
+
     @staticmethod
     def rand(nr_bytes):
         """
@@ -135,3 +241,73 @@ class HexData:
         data = np.frombuffer(np.random.bytes(nr_bytes), dtype=np.uint8)
 
         return HexData(value=data)
+
+    def __len__(self):
+        return self._data.shape[0]
+
+    def __getitem__(self, index):
+        # Check type index
+        if isinstance(index, list):
+            pass
+        elif isinstance(index, np.ndarray):
+            pass
+        elif isinstance(index, slice):
+            pass
+        elif isinstance(index, int):
+            pass
+        else:
+            raise ValueError("This type is not allowed")
+
+        return HexData(self._data[index])
+
+    def __setitem__(self, index, value):
+        # Check type index
+        if isinstance(index, list):
+            pass
+        elif isinstance(index, np.ndarray):
+            pass
+        elif isinstance(index, slice):
+            pass
+        elif isinstance(index, int):
+            pass
+        else:
+            raise ValueError("This type is not allowed")
+
+        # Check type value
+        if isinstance(value, list):
+            value = HexData(value)
+        elif isinstance(value, np.ndarray):
+            value = HexData(value)
+        elif isinstance(value, int):
+            value = HexData(value)
+        elif isinstance(value, HexData):
+            pass
+        else:
+            raise ValueError("This type is not allowed")
+
+        # Check sizes are equal
+        if isinstance(index, list) or isinstance(index, np.ndarray):
+            if len(index) != len(value):
+                raise ValueError("The sizes of index and values must be the same")
+        elif isinstance(index, int):
+            if len(value) != 1:
+                raise ValueError("The sizes of index and values must be the same")
+        elif isinstance(index, slice):
+            size_slice = len(range(*index.indices(self._data.shape[0])))
+            if size_slice != len(value):
+                raise ValueError("The sizes of index and values must be the same")
+
+        self._data[index] = value.get_value()
+
+    def __eq__(self, other):
+        if isinstance(other, HexData):
+            if len(self) != len(other):
+                return False
+            return np.alltrue(np.equal(self.get_value(), other.get_value()))
+        else:
+            return self.to_number() == HexData(other).to_number()
+
+    def __add__(self, other):
+        if not isinstance(other, HexData):
+            raise ValueError("One parameter is not a HexData object")
+        return HexData(np.concatenate((self.get_value(), other.get_value())))
